c7770edf856c78ec34a27f3b017804f7afb93058
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-12 20:20:45 +0200

    mvn i

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,8 +1,7 @@
 void call(){
     stage("Maven: Build"){
         echo "building jar from jte"
-        maven.run(["clean", "install"])
-        // bat "mvn clean install"
+        maven.run(["clean", "install"], profiles: ["integration-test"])        // bat "mvn clean install"
         
     }
 }
