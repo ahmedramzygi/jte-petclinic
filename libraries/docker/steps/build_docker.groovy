d8af9ed59d861b0fd850425719c929958c93ad6e
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-12 22:39:25 +0200

    node

--- libraries/docker/steps/build_docker.groovy
+++ libraries/docker/steps/build_docker.groovy
@@ -1,11 +1,13 @@
 void call(){
     stage("Docker: Build docker image ")
     {
+        node{
             echo "building the docker image from jte..."
             withCredentials([usernamePassword(credentialsId:'docker-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
             bat "docker login -u $USER -p $PASS"
             bat 'docker-compose up -d'
             bat 'docker-compose push'   
+            }
      }
 }
 }
