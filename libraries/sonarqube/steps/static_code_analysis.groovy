16a57ea5a1b6c4a6da2d0502fab86e3163f0b5dd
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-13 16:53:35 +0200

    sqmvn

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -3,7 +3,7 @@ void call(){
       node{
         echo 'Doing the Static Code Analysis from jte by SQ'
         withSonarQubeEnv(installationName: 'sq1') { 
-          bat './mvnw clean org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar'
+          bat 'mvn clean package sonar:sonar'
         }  
           }
     }
