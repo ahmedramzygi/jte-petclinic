72213b98f8345740774596ff8b038dc4fdb882d1
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-13 16:49:12 +0200

    sq1

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,6 +1,10 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
+      node{
+        echo 'Doing the Static Code Analysis from jte by SQ'
         withSonarQubeEnv(installationName: 'sq1') { 
           bat './mvnw clean org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.0.2155:sonar'
-        }    }
+        }  
+          }
+    }
 }
