585da0846a6e66a09a8f885be8048434ce5110a8
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-14 14:04:33 +0200

    adding mysql

--- libraries/docker/steps/build_docker.groovy
+++ libraries/docker/steps/build_docker.groovy
@@ -4,10 +4,8 @@ void call(){
         node{
             echo "building the docker image from jte..."
             withCredentials([usernamePassword(credentialsId:'docker-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
-            bat "docker login -u $USER -p $PASS"
-            bat 'docker ps -a'            
-            bat 'docker-compose -f docker-compose.yaml up'
-            bat 'docker ps -a'
+            bat "docker login -u $USER -p $PASS"         
+            bat 'docker-compose up -d'
             // bat 'docker-compose push'   
             }
      }
