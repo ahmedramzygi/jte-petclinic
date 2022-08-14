8b171f721bd7c8babe6bb44fd439e2ff130b3300
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-14 12:40:09 +0200

    commands to debug

--- libraries/docker/steps/build_docker.groovy
+++ libraries/docker/steps/build_docker.groovy
@@ -5,7 +5,9 @@ void call(){
             echo "building the docker image from jte..."
             withCredentials([usernamePassword(credentialsId:'docker-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
             bat "docker login -u $USER -p $PASS"
+            bat 'docker ps -a'            
             bat 'docker-compose up -d'
+            bat 'docker ps -a'
             // bat 'docker-compose push'   
             }
      }
