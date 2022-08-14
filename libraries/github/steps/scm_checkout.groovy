bbd3121466f1365684e68ad096601f2025681dd9
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-14 18:01:22 +0200

    scm checkout

--- libraries/github/steps/scm_checkout.groovy
+++ libraries/github/steps/scm_checkout.groovy
@@ -1,11 +1,20 @@
+import static io.wcm.devops.jenkins.pipeline.utils.ConfigConstants.*
+
 void call(){
     stage("Checkout: Checkout the code from github ")
     {
         node{
-            echo "Checkout the code from github"
-            withCredentials([usernamePassword(credentialsId:'github-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
-            bat 'git clone https://github.com/ahmedramzygi/petclinic C:/ProgramData/Jenkins/.jenkins/workspace/jte-petclinic-task_main'
+            
+            checkoutScm( 
+                (SCM) : [
+                    (SCM_URL) : "https://github.com/ahmedramzygi/petclinic",
+                ]
+            )
+            
+            // echo "Checkout the code from github"
+            // withCredentials([usernamePassword(credentialsId:'github-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
+            // bat 'git clone https://github.com/ahmedramzygi/petclinic C:/ProgramData/Jenkins/.jenkins/workspace/jte-petclinic-task_main'
             }
      }
 }
-}
+
