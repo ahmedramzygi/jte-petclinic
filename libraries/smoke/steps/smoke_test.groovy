8c042595d7051db549a1dbdb3ad680a76cbe8b8d
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-08-18 10:47:45 +0200

    curl command

--- libraries/smoke/steps/smoke_test.groovy
+++ libraries/smoke/steps/smoke_test.groovy
@@ -3,7 +3,7 @@ void call(){
     stage("Smoke test:Test the pet clinic application ")
     {      node{
             echo "Smoke test the pet clinic application "
-            bat 'curl -I http://localhost:8081/'
+            bat 'curl http://localhost:8081/'
             }
             }
            
