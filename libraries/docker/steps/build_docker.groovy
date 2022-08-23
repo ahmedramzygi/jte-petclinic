b72d3433a72112adfd598460d37ed628b2ce1c3a
ahmedramzygi <ramzy@AHMED-PC.localdomain>
2022-08-23 15:15:42 +0200

    docker credential

--- libraries/docker/steps/build_docker.groovy
+++ libraries/docker/steps/build_docker.groovy
@@ -4,7 +4,7 @@ void call(){
     {
         node{
             echo "building the docker image from jte..."
-            withCredentials([usernamePassword(credentialsId:'docker-for-jenkins',passwordVariable:'PASS',usernameVariable:'USER')]){
+            withCredentials([usernamePassword(credentialsId:'docker-hub',passwordVariable:'PASS',usernameVariable:'USER')]){
             bat "docker login -u $USER -p $PASS"         
             bat 'docker-compose up -d'
             bat 'docker-compose push'   
