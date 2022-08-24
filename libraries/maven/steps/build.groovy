750288bd1c8d81b773a6fe2be154d68e0e6b2671
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-08-24 22:10:54 +0200

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -4,7 +4,7 @@ void call(){
     {
         node('maven-agent'){
         echo "building jar from jte"     
-        
+            sh 'cd jte-petclinic_main'
             sh 'mvn clean install'
            
     }
