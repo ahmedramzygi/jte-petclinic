cb28af6bf2492147722efaebfb2aa449dc33d909
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-08-24 15:02:31 +0200

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,19 +1,19 @@
 libraries{
-    // github
-    // maven
-   // sonarqube
-   // docker
-    ansible
+    github
+    maven
+    // sonarqube
+    // docker
+    // ansible
     // smoke
     
 }
 stages{
     continuous_integration{
-        // scm_checkout
-        // build
-      //  static_code_analysis
+        scm_checkout
+        build
+        //  static_code_analysis
         // build_docker
-        ansible
+        //ansible
         // smoke_test
         
     }
