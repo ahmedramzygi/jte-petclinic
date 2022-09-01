35060107bd9c0208a1904be3794b13d72d178f83
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-09-01 10:49:58 +0200

    Update build.groovy

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -2,7 +2,7 @@ void call(){
 
     stage("Maven: Build the jar file")
     {
-        node('maven-agent'){
+        node('master'){
         echo "building jar from jte"     
             sh 'mvn clean install'
            
