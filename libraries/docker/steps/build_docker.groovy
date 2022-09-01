1d3a9801731266547242ac6de35da5cc5a735ee1
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-09-01 14:03:05 +0200

    Update build_docker.groovy

--- libraries/docker/steps/build_docker.groovy
+++ libraries/docker/steps/build_docker.groovy
@@ -4,6 +4,7 @@ void call(){
          {
                  node('docker-agent'){
             echo "building the docker image from jte..."
+            unstash 'targetfiles'
             withCredentials([usernamePassword(credentialsId:'docker-hub',passwordVariable:'PASS',usernameVariable:'USER')]){
             sh "docker login -u $USER -p $PASS"         
             sh 'docker-compose up -d'
