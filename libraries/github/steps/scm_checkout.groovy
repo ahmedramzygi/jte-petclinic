d09880864ac2a54aef76e9329c07cb6739f1da51
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-09-03 20:17:52 +0200

    Update scm_checkout.groovy

--- libraries/github/steps/scm_checkout.groovy
+++ libraries/github/steps/scm_checkout.groovy
@@ -3,10 +3,10 @@
 void call(){
 
     stage("SCM Checkout: Checkout the code from github ")
-    {      node ('git-agent'){
+    {      
             echo "Checkout the code from github"
             checkout scm
             }
-            }
+            
            
      }
