ff42e7e127f135dcd7997044c4f13336635962bb
Ahmed Ramzy <40033492+ahmedramzygi@users.noreply.github.com>
2022-09-08 13:10:29 +0200

    Update static_code_analysis.groovy

--- libraries/sonarqube/steps/static_code_analysis.groovy
+++ libraries/sonarqube/steps/static_code_analysis.groovy
@@ -1,9 +1,9 @@
 void call(){
     stage("SonarQube: Static Code Analysis"){
-      node(label 'maven-agent'){
+      node( 'maven-agent'){
         echo 'Doing the Static Code Analysis from jte by SQ'
         withSonarQubeEnv(installationName: 'sq1') { 
-          bat 'mvn clean package sonar:sonar'
+          sh 'mvn clean package sonar:sonar'
         }  
           }
     }
