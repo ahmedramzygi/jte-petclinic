00859784386ada03d724455c175ea68b8dd24436
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-09-11 12:13:27 +0200

    scm

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -2,7 +2,7 @@ void call(){
 
     stage("Maven: Build the jar file")
     {
-        checkout scm
+        // checkout scm
         node('maven-agent'){
         echo "building jar from jte"    
             sh 'mvn clean install'
