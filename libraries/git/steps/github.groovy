1d988a03703efd10d7d41fde668d4a51cf5abeb7
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-09-11 10:54:38 +0200

    add

--- libraries/git/steps/github.groovy
+++ libraries/git/steps/github.groovy
@@ -17,8 +17,9 @@ def get_source_branch(){
   def cred_id = env.GIT_CREDENTIAL_ID
   
 
-  withCredentials([usernamePassword(credentialsId: cred_id, passwordVariable: 'PAT', usernameVariable: 'USER')]) {
-      return GitHub.getRepository("${env.ORG_NAME}/${env.REPO_NAME}")
+  withCredentials([usernamePassword(credentialsId: 'cred_id', passwordVariable: 'PAT', usernameVariable: 'USER')]) {
+      return GitHub.connectUsingOAuth(PAT).
+              getRepository("${env.ORG_NAME}/${env.REPO_NAME}")
               .getPullRequest(env.CHANGE_ID.toInteger())
               .getHead()
               .getRef()
