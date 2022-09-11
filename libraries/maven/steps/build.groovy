8dfe838b44289e2caa06d4c67a3ff767703bb7fd
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-09-11 12:09:17 +0200

    fix the scm

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -2,9 +2,9 @@ void call(){
 
     stage("Maven: Build the jar file")
     {
+        checkout scm
         node('maven-agent'){
         echo "building jar from jte"    
-            checkout scm
             sh 'mvn clean install'
            // stash includes: 'target/*.jar', name: 'targetfiles'
            
