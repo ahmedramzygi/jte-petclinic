a767bef84847749a554df1a55c3718ecb05e492a
ahmedramzygi <17P8229@eng.asu.edu.eg>
2022-09-14 14:24:15 +0200

    add

--- libraries/git/steps/on_merge.groovy
+++ libraries/git/steps/on_merge.groovy
@@ -75,7 +75,7 @@ String get_merged_from(){
 }
 
 String get_feature_branch_sha(){
-  node("${config.agent}"){
+  node("${agent}"){
     unstash "workspace"
     sh(
       script: "git rev-parse \$(git --no-pager log -n1 | grep Merge: | awk '{print \$3}')",
